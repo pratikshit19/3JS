3dd07895fd4d102f016bdb518ad58043d33dacd3
pratikshit19 <pratikshit@icloud.com>
2023-07-08 23:36:55 +0530

    completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -57,7 +57,7 @@ const Contact = () => {
   }
 
   return (
-    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden p-20'>
+    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden '>
      <motion.div
      variants={slideIn('left', "tween", 0.2, 1)}
      className='flex-[0.75] black-gradient p-8 rounded-2xl'
